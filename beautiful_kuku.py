513bf4f0bee85a0a6257d84fb76ee9182d029939
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-22 11:22:27 +0900

    B-3手直し

---
+++ beautiful_kuku.py
@@ -0,0 +1,7 @@
+x = int(input("行数を入力してください: "))
+y = int(input("列数を入力してください: "))
+
+for x in range(1, x + 1):
+    for y in range(1, y + 1):
+        print(" {} ✕  {} ＝ {:2d} ".format(x, y, x * y), end="|")
+    print()
